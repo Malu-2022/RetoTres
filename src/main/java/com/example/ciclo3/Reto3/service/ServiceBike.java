b5e9b320787612b1183745562a9805f814c256f5
Malu-2022 <113222933+Malu-2022@users.noreply.github.com>
2022-10-07 19:03:28 -0500

    Update ServiceBike.java

--- src/main/java/com/example/ciclo3/Reto3/service/ServiceBike.java
+++ src/main/java/com/example/ciclo3/Reto3/service/ServiceBike.java
@@ -38,5 +38,30 @@ public class ServiceBike {
             }
         }
     }
+    
+    public Bike update(Bike bike){
+        if(bike.getId()!=null){
+            Optional<Bike>g= repositoryBike.getBike(bike.getId());
+            if(!g.isEmpty()){
+                if(bike.getDescription()!=null){                    
+                    g.get().setDescription(bike.getDescription());
+                }
+                if(bike.getName()!=null){
+                    g.get().setName(bike.getName());
+                }
+                return repositoryBike.save(g.get());
+            }
+        }
+        return bike;
+    }
+
+    public boolean deleteBike(int id){
+        Boolean d= getBike(id).map(bike -> {
+                                                repositoryBike.delete(bike);
+                                                return true;
+                                            }
+                                      ).orElse(false);
+        return d;
+    }
 
 }
